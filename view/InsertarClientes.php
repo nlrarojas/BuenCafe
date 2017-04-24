f6820cda82055b99d9fd8b96d1ba46a05399a6b4
JCM <jcarlosmc1994@gmail.com>
2017-04-24 02:17:19 -0600

    interfaz

--- view/InsertarClientes.php
+++ view/InsertarClientes.php
@@ -7,27 +7,41 @@ include_once 'header.php';
     <br/><br/><br/><br/><br/>
     <section id="principal">
         <article id="contenidoIngresoAsociado">
-            <div class="boxed" id="menuIngresoAsociado"><br/>
-                <span><a href="?ModificarCliente"><div>Modificar un cliente</div></a></span><br/>
-                <span><a href="?BuscarCliente"><div>Buscar cliente</div></a></span><br/>
-                <span><a href="?EliminarCliente"><div>Eliminar cliente</div></a></span>
-            </div>
-            <div class="boxed" id="divContenedorIngreso">
-                <br/><br/><br/>
-                <form action="?InsertarClientes=ingresar" method="post">
-                    <label>Ingrese los datos del cliente:</label><br/><br/><br/>
-                    <label for="tApellido2">Cédula</label>
-                    <input type="text" id="tCedula" name="tCedula" required/><br/><br/>                    
-                    <label for="tNombre">Nombre</label>
-                    <input type="text" id="tNombre" name="tNombre" required/><br/><br/>
-                    <label for="tApellido1">Apellidos</label>
-                    <input type="text" id="tApellidos" name="tApellidos" required/><br/><br/>                    
-                    <label for="tDireccion">Fecha de nacimiento</label><br/><br/>
-                    <input type="date" id="tFecha" name="tFecha" required/><br/><br/>
-                    
-                    <input type="submit" id="bIngresar" name="bIngresar" value="Ingresar"/>
-                </form>
-                <br/><br/><br/>
+            <div class="row">
+                <div class="col-sm-3"></div>
+                <div class="col-sm-2">
+                    <div class="form-group">    
+                        <span><a class="btn btn-primary" href="?ModificarCliente"><div>Modificar cliente</div></a></span><br/>
+                        <br>
+                        <span><a class="btn btn-primary" href="?BuscarCliente">   <div>Buscar  cliente</div></a></span><br/>
+                        <br>
+                        <span><a class="btn btn-primary" href="?EliminarCliente"> <div>Eliminar cliente</div></a></span>
+                    </div>
+                </div> <div class="col-sm-3">
+                    <div class="boxed" id="divContenedorIngreso">
+                        <br/><br/><br/>
+
+                        <form action="?InsertarClientes=ingresar" method="post">
+                            <div class="form-group">
+                                <label>Ingrese los datos del cliente:</label>
+                                <label for="tApellido2">Cédula</label>
+                                <input  class="form-control" type="text" id="tCedula" name="tCedula" required/>                  
+                            </div><div class="form-group">
+                                <label for="tNombre">Nombre</label>
+                                <input  class="form-control" type="text" id="tNombre" name="tNombre" required/>
+                            </div><div class="form-group">
+                                <label for="tApellido1">Apellidos</label>
+                                <input  class="form-control" type="text" id="tApellidos" name="tApellidos" required/>                   
+                            </div><div class="form-group">
+                                <label for="tDireccion">Fecha de nacimiento</label>
+                                <input  class="form-control" type="date" id="tFecha" name="tFecha" required/>
+                            </div>
+                            <input class="btn btn-default" type="submit" id="bIngresar" name="bIngresar" value="Ingresar"/>
+                        </form>
+
+                        <br/><br/><br/>
+                    </div>
+                </div>
             </div>
         </article>		
     </section>
@@ -35,5 +49,6 @@ include_once 'header.php';
 </section>
 
 <?php
+
 include_once 'footer.php';
 ?>                    
