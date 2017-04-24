1a21650dd38808e2c6a719dfe4ac9bd0c7f0ef8e
JCM <jcarlosmc1994@gmail.com>
2017-04-24 02:38:03 -0600

    factura cliente

--- view/ModificarClientes.php
+++ view/ModificarClientes.php
@@ -1,5 +1,4 @@
 <?php
-
 include_once 'header.php';
 ?>
 
@@ -14,22 +13,41 @@ include_once 'header.php';
             </div>
             <div class="boxed" id="divContenedorIngreso">
                 <br/><br/><br/>
-                <form action="?ModificarCliente=modificar" method="post">
-                    <label>Ingrese la cédula del cliente para modificar:</label><br/><br/><br/>
-                    
-                    <form action="?ModificarCliente=buscar" method="post">
+                <form action="?ModificarCliente=buscar" method="post">
+                        <label>Ingrese la cédula del cliente para modificar:</label><br/><br/><br/>
                         <label for="tCedula">Cédula</label>
-                        <input type="submit" id="bIngresar" name="bIngresar" value="Ingresar"/>
+                        <?php
+                        if (isset($resultadoBusqueda)) {
+                            ?>
+                            <input type="text" id="tCedula" name="tCedula" value="<?php echo $resultadoBusqueda->getCedula(); ?>"/><br/><br/>
+                            <?php
+                        }else{
+                            ?>
+                            <input type="text" id="tCedula" name="tCedula" required/><br/><br/>
+                            <?php
+                        }
+                        ?>
+                        <input type="submit" id="bIngresar" name="bIngresar" value="Buscar"/><br/><br/><br/>
                     </form>
-                    <input type="text" id="tCedula" name="tCedula" required/><br/><br/>                    
-                    <label for="tNombre">Nombre</label>
-                    <input type="text" id="tNombre" name="tNombre" required/><br/><br/>
-                    <label for="tApellidos">Apellidos</label>
-                    <input type="text" id="tApellidos" name="tApellidos" required/><br/><br/>                    
-                    <label for="tFecha">Fecha de nacimiento</label><br/><br/>
-                    <input type="date" id="tFecha" name="tFecha" required/><br/><br/>
-                    
-                    <input type="submit" id="bIngresar" name="bIngresar" value="Modificar"/>
+                <form action="?ModificarCliente=modificar&cedula=<?php echo $resultadoBusqueda->getCedula(); ?>" method="post">
+                    <?php
+                    if (isset($resultadoBusqueda)) {
+                        ?>
+                        <label for="tNombre">Nombre</label>
+                        <input type="text" id="tNombre" name="tNombre" value="<?php echo $resultadoBusqueda->getNombre(); ?>"/><br/><br/>
+                        <label for="tApellidos">Apellidos</label>
+                        <input type="text" id="tApellidos" name="tApellidos" value="<?php echo $resultadoBusqueda->getApellidos(); ?>"/><br/><br/>
+                        <label for="tFecha">Fecha de nacimiento</label><br/><br/>
+                        <input type="date" id="tFecha" name="tFecha" value="<?php echo $resultadoBusqueda->getFechaNacimiento(); ?>"/><br/><br/>
+                        <label for="tPuntaje">Puntaje acumulado</label>
+                        <input type="text" id="tPuntaje" name="tPuntaje" value="<?php echo $resultadoBusqueda->getPuntajeAcumulado(); ?>"/><br/><br/>
+                        <label for="tApellidos">Premios canjeados</label>
+                        <input type="text" id="tPremios" name="tPremios" value="<?php echo $resultadoBusqueda->getPremiosCanjeados(); ?>"/><br/><br/>                        
+
+                        <input type="submit" id="bIngresar" name="bIngresar" value = "Modificar"/>
+                        <?php
+                    }
+                    ?>
                 </form>
                 <br/><br/><br/>
             </div>
