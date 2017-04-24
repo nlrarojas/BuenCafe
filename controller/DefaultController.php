254350b1ec483504e561cd74fd774a31d91d8e29
JCM <jcarlosmc1994@gmail.com>
2017-04-24 02:37:45 -0600

    factura cliente

--- controller/DefaultController.php
+++ controller/DefaultController.php
@@ -23,9 +23,26 @@ class DefaultController {
             include 'view/InsertarClientes.php';
         }
         if (isset($_GET['ModificarCliente'])) {
+            if($_GET["ModificarCliente"] == "modificar"){
+                $cedulaCliente = $_GET["cedula"];
+                $clienteModificar = new Cliente($cedulaCliente, $_POST["tNombre"], $_POST["tApellidos"], $_POST["tFecha"], $_POST["tPuntaje"], $_POST["tPremios"]);
+                $this->model->modificarCliente($clienteModificar);
+            }
+            if($_GET["ModificarCliente"] == "buscar"){
+                $cedulaCliente = $_POST["tCedula"];
+                $resultadoBusqueda = $this->model->buscarCliente($cedulaCliente);
+            }
             include 'view/ModificarClientes.php';
         }
         if (isset($_GET['EliminarCliente'])) {
+            if($_GET["EliminarCliente"] == "eliminar"){
+                $cedulaCliente = $_GET["cedula"];                
+                $this->model->eliminarCliente($cedulaCliente);
+            }
+            if($_GET["EliminarCliente"] == "buscar"){
+                $cedulaCliente = $_POST["tCedula"];
+                $resultadoBusqueda = $this->model->buscarCliente($cedulaCliente);
+            }
             include 'view/EliminarClientes.php';
         }
         
@@ -40,14 +57,26 @@ class DefaultController {
         
         if (isset($_GET['BuscarCliente'])) {
             if($_GET["BuscarCliente"] == "buscar"){
-                $cedulaCliente = $_POST["tCedula"];
-                if($cedulaCliente == ""){
-                    $resultadoBusqueda = $this->model->buscarTodosLosClientes();
-                }else{
+                if(isset($_GET["modificarCliente"])){
+                    $cedulaCliente = $_GET["modificarCliente"];
                     $resultadoBusqueda = $this->model->buscarCliente($cedulaCliente);
+                    include 'view/ModificarClientes.php';
+                }else if(isset($_GET["eliminarCliente"])){
+                    $cedulaCliente = $_GET["eliminarCliente"];
+                    $resultadoBusqueda = $this->model->buscarCliente($cedulaCliente);
+                    include 'view/EliminarClientes.php';
+                }else{
+                    $cedulaCliente = $_POST["tCedula"];
+                    if($cedulaCliente == ""){
+                        $resultadoBusqueda = $this->model->buscarTodosLosClientes();                    
+                    }else{
+                        $resultadoBusqueda = $this->model->buscarCliente($cedulaCliente);
+                    }
+                    include 'view/BuscarClientes.php';
                 }
+            }else{
+                include 'view/BuscarClientes.php';
             }
-            include 'view/BuscarClientes.php';
         } else {
             include 'view/indexView.php';
         }
